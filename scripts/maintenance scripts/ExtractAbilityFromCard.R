0e91fec63f876975a01ce9c4b5a1d5a536f74570
Frederic Kruger <kruger.frederic@gmail.com>
2025-07-14 13:42:21 +0000

    Refactor code structure for improved readability and maintainability

--- scripts/maintenance scripts/ExtractAbilityFromCard.R
+++ scripts/maintenance scripts/ExtractAbilityFromCard.R
@@ -1,34 +1,47 @@
 library(magick)
 library(grid)
 
-folder_path <- "E:\\Documents\\Code\\Phaser\\PhaserOP\\client\\assets\\cardart\\"
+folder_path <- "C:\\Users\\Kruger_F1\\Pictures\\tmp\\"
 files <- list.files(folder_path)
 
-file <- "ST01-017.png"
+filename <- "ST04-017"
+file <- paste0(filename, ".png")
 
 img <- image_read(paste0(folder_path, file))
 
 img_info <- image_info(img)
 width <- img_info$width
 height <- img_info$height
- 
+
 # #ABILIY 1 line top transparent
 # x_center <- 300# X-coordinate of the circle center
 # y_center <- 538
 # x_radius <- 522
 # y_radius <- 45
 
-# #ABILIY 2 line top transparent
+#ABILIY 2 line top transparent
 # x_center <- 300# X-coordinate of the circle center
 # y_center <- 548
 # x_radius <- 522
 # y_radius <- 65
- 
+
 # #ABILIY 3 line top transparent
 # x_center <- 300# X-coordinate of the circle center
 # y_center <- 560
 # x_radius <- 522
 # y_radius <- 90
+
+#ABILIY 4 line top transparent
+# x_center <- 300# X-coordinate of the circle center
+# y_center <- 577
+# x_radius <- 522
+# y_radius <- 125
+
+#ABILIY 5 line top transparent
+# x_center <- 300# X-coordinate of the circle center
+# y_center <- 590
+# x_radius <- 522
+# y_radius <- 150
 
 # #ABILIY 6 line top transparent
 # x_center <- 300# X-coordinate of the circle center
@@ -41,7 +54,7 @@ height <- img_info$height
 # y_center <- 520
 # x_radius <- 522
 # y_radius <- 45
-#  
+  
 # #ABILIY 2 line top
 # x_center <- 300# X-coordinate of the circle center
 # y_center <- 532
@@ -49,10 +62,16 @@ height <- img_info$height
 # y_radius <- 65
 
 #ABILIY 1 3 linestop
-x_center <- 300# X-coordinate of the circle center
-y_center <- 545
-x_radius <- 522
-y_radius <- 90
+# x_center <- 300# X-coordinate of the circle center
+# y_center <- 545
+# x_radius <- 522
+# y_radius <- 90
+
+#ABILIY 1 4 linestop
+# x_center <- 300# X-coordinate of the circle center
+# y_center <- 558
+# x_radius <- 522
+# y_radius <- 120
 
 # #ABILIY 1 6 linestop
 # x_center <- 300# X-coordinate of the circle center
@@ -60,7 +79,7 @@ y_radius <- 90
 # x_radius <- 522
 # y_radius <- 180
 
-# #TRIGGER 1 1 linestop
+#TRIGGER 1 1 linestop
 # x_center <- 300# X-coordinate of the circle center
 # y_center <- 675
 # x_radius <- 522
@@ -78,6 +97,17 @@ y_radius <- 90
 # x_radius <- 522
 # y_radius <- 65
 
+# #SECOND ABILITY 3 line top transparent
+# x_center <- 300# X-coordinate of the circle center
+# y_center <- 619
+# x_radius <- 522
+# y_radius <- 90
+
+#SECOND ABILITY 1 line top transparent
+# x_center <- 300# X-coordinate of the circle center
+# y_center <- 592
+# x_radius <- 522
+# y_radius <- 35
 
 mask <- image_draw(image_blank(width = width, height = height, color = "none"))
 polygon(x = c(x_center - x_radius/2, x_center + x_radius/2, x_center + x_radius/2, x_center - x_radius/2), 
@@ -92,12 +122,12 @@ img_cropped <- image_composite(img, mask, operator = "copyopacity")
 geom <- paste0(x_radius, "x", y_radius, "+", x_center-x_radius/2, "+", y_center-y_radius/2)
 resized_img <- image_crop(img_cropped, geometry = geom)
 print(resized_img)
-image_write(resized_img, paste0("c:/Users/Freddy/Desktop/temp.png"))
+image_write(resized_img, paste0("C:/Users/Kruger_F1/Documents/temp.png"))
 
 
 
 
-img_cropped <- image_read(paste0("c:/Users/Freddy/Desktop/temp.png"))
+img_cropped <- image_read(paste0("C:/Users/Kruger_F1/Documents/temp.png"))
 print(img_cropped)
 img_info <- image_info(img_cropped)
 width <- img_info$width
@@ -108,4 +138,5 @@ dev.off()
 img_rounded <- image_composite(img_cropped, mask, operator = "copyopacity")
 print(img_rounded)
 
-image_write(img_rounded, paste0("c:/Users/Freddy/Desktop/abilityart/", "ST01-017-ABILITY1.png"))
+image_write(img_rounded, paste0("C:/Users/Kruger_F1/Documents/", paste0(filename, "-ABILITY1.png")))
+# image_write(img_rounded, paste0("C:/Users/Kruger_F1/Documents/", paste0(filename, "-TRIGGER.png")))
